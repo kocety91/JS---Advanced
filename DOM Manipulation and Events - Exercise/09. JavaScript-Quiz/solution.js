572621c8b19f6a47033af8e04ac38a4178b6458e
kocety91 <kocety91@gmail.com>
2021-09-26 14:05:05 +0300

    Advanced Functions - Lab

--- DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
+++ DOM Manipulation and Events - Exercise/09. JavaScript-Quiz/solution.js
@@ -1,3 +1,11 @@
 function solve() {
-  //TODO...
+ let sections = document.querySelectorAll('#quizzie section');
+
+ sections.forEach(section => {
+   sections.addEventListener('click',function(e) {
+    
+   });
+ });
+ 
+
 }
